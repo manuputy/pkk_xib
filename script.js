1f60aca2cfa859a1bc2118d5cf3c2d85b8d2620a
manuputy <127161025+manuputy@users.noreply.github.com>
2023-09-25 13:37:53 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,41 @@
-let displayValue = '';
+document.addEventListener("DOMContentLoaded", function() {
+    const taskList = document.getElementById("task-list");
+    const newTaskInput = document.getElementById("new-task");
+    const addTaskButton = document.getElementById("add-task");
 
-function calculate(button) {
-    const value = button.innerText;
-
-    if (value === '=') {
-        try {
-            displayValue = eval(displayValue);
-        } catch (error) {
-            displayValue = 'Error';
+    addTaskButton.addEventListener("click", function() {
+        const taskText = newTaskInput.value.trim();
+        if (taskText !== "") {
+            addTask(taskText);
+            newTaskInput.value = "";
         }
-    } else if (value === 'C') {
-        displayValue = '';
-    } else {
-        displayValue += value;
-    }
+    });
 
-    document.getElementById('display').value = displayValue;
-}
+    newTaskInput.addEventListener("keyup", function(event) {
+        if (event.key === "Enter") {
+            addTaskButton.click();
+        }
+    });
+
+    function addTask(taskText) {
+        const taskItem = document.createElement("li");
+        taskItem.textContent = taskText;
+
+        // Tambahkan tombol delete ke setiap tugas
+        const deleteButton = document.createElement("button");
+        deleteButton.textContent = "Bisa di delete da";
+        deleteButton.classList.add("delete-button");
+
+        deleteButton.addEventListener("click", function() {
+            taskList.removeChild(taskItem);
+        });
+
+        taskItem.appendChild(deleteButton);
+
+        taskItem.addEventListener("click", function() {
+            taskItem.classList.toggle("completed");
+        });
+
+        taskList.appendChild(taskItem);
+    }
+});
